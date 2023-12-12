76d662477c00d57f59f62e4f352040dd7fcd3349
ABleasdale1 <112654381+ABleasdale1@users.noreply.github.com>
2023-12-12 16:50:42 +0000

    Customer model

--- gameTrends/userApp/models.py
+++ gameTrends/userApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fName = models.CharField(max_length=50)
+    lName = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+
